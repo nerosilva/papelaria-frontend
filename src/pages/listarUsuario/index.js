cc7caeef5625e7a37fe88d8e4abe1b5f6996a07a
nerosilva <jaldevan2014@gmail.com>
2023-11-24 21:21:56 -0300

    alterado lista de usuarios

--- src/pages/listarUsuario/index.js
+++ src/pages/listarUsuario/index.js
@@ -6,15 +6,48 @@ import Menu from '../../componetes/menu'
 
 export default function Listausuario() {
 
+  const dados=[
+          
+            {id:1,nome:"Nero",email:"Jaldevan2014@gmail.com"},
+            {id:2,nome:"Silva",email:"Nerinho0007@gmail.com"},
+            {id:3,nome:"Caue",email:"Cauenero007@gmail.com"},
+               
+  ]
+
   return (
     <div className="dashboard-container">
       <div className='menu'>
 
-         <Menu />
+        <Menu />
 
       </div>
       <div className='Principal'>
         <h1>lista de Usuario</h1>
+        <table border={1}>
+          <tr>
+
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th></th>
+
+          </tr>
+          {
+             dados. map((usu)=>{
+              return(
+                <tr key={usu.toString}>
+                  <td>{usu.id}</td>
+                  <td>{usu.nome}</td>
+                  <td>{usu.email}</td>
+
+                </tr>
+              )
+             })
+
+          }
+
+
+        </table>
       </div>
 
     </div>
