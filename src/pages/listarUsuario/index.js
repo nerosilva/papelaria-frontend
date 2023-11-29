41607b6107088b2e03c9ed241572abb914691193
nerosilva <jaldevan2014@gmail.com>
2023-11-28 21:31:24 -0300

    novo cadastro

--- src/pages/listarUsuario/index.js
+++ src/pages/listarUsuario/index.js
@@ -2,6 +2,10 @@ import '../../pages/global.css'
 import Logo from '../../assets/img/logo.jpg'
 import Menu from '../../componetes/menu'
 import { FiEdit, FiTrash, FiDelete, FiFilePlus } from "react-icons/fi";
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import { Link } from 'react-router-dom';
+import Head from '../../componetes/Head';
 
 export default function Listausuario() {
 
@@ -11,18 +15,36 @@ export default function Listausuario() {
     { id: 2, nome: "Silva", email: "Nerinho0007@gmail.com" },
     { id: 3, nome: "Caue", email: "Cauenero007@gmail.com" },
   ]
+  
+  const apagar = (id) => {
+    confirmAlert({
+      title: 'Excluir Usuario',
+      message: 'Deseja realmente excluir esse Usuario.',
+      buttons: [
+        {
+          label: 'Sim',
+          onClick: () => alert(` Voce apagou o usuario id:${id}`)
+        },
+        {
+          label: 'Nao',
+          onClick: () => alert('Click No')
+        }
+      ]
+    });
+  };
 
   return (
     <div className="dashboard-container">
+      
       <div className='menu'>
 
         <Menu />
 
       </div>
       <div className='Principal'>
-        <h1>lista de Usuario</h1>
-        <button className='btn-novo'>Novo Cadastro</button>
-        <table>
+      <Head title='lista de Usuario'/>
+        <Link className='btn-novo'>Novo Cadastro</Link>
+        <table className="table">
           <tr>
 
             <th>Id</th>
@@ -44,7 +66,11 @@ export default function Listausuario() {
                     <FiEdit size={18} color='#3a5795' />
                   </td>
                   <td className='botoes'>
-                    <FiTrash size={18} color='red'/>
+                    <FiTrash 
+                    size={18} 
+                    color='red'
+                    onClick={(e)=>apagar(usu.id)}
+                    />
                   </td>
 
                 </tr>
