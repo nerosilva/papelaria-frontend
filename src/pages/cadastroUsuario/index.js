a89fa39ae21a85391aefa5e32876f6741731a997
nerosilva <jaldevan2014@gmail.com>
2023-12-05 21:32:39 -0300

    adicionamos o localStorege

--- src/pages/cadastroUsuario/index.js
+++ src/pages/cadastroUsuario/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 import '../../pages/global.css'
 import Menu from '../../componetes/menu'
 import { FiFilePlus } from "react-icons/fi";
@@ -11,6 +10,24 @@ import { MdOutlineCancel } from "react-icons/md";
 
 export default function Cadastrousuario() {
     const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const usuario = {
+        nome,
+        email,
+        senha,
+
+    }
+    function salvardados(e) {
+        
+        e.preventDefault();
+       // console.log(usuario)
+       const banco =JSON.parse(localStorage.getItem("cd-usuarios") || "[]");
+       banco.push(usuario);
+       localStorage.setItem("cd-usuarios", JSON.stringify(banco));
+       alert("Usuario salvo com sucesso");
+
+    }
 
     return (
         <div className="dashboard-container">
@@ -21,23 +38,35 @@ export default function Cadastrousuario() {
             <div className='Principal'>
                 <Head title='cadastro de Usuario' />
                 <div className='form-container'>
-                    <form className='form-cadastro'>
-                        <input type='text' value={nome} onChange={e => setNome(e.target.value)}
+                    <form className='form-cadastro' onSubmit={salvardados}>
+                        <input type='text'
+                            value={nome}
+                            onChange={e => setNome(e.target.value)}
                             placeholder='Digite o nome do usuario' />
-                        <input type='email' placeholder='Digite o email' />
-                        <input type='password' placeholder='Digite a senha' />
+
+                        <input type='email'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            placeholder='Digite o email' />
+
+                        <input type='password'
+                            value={senha}
+                            onChange={e => setSenha(e.target.value)}
+                            placeholder='Digite a senha' />
+
                         <div className='acao'>
-                            <botton className='btn-save'>
+                            <button className='btn-save'>
                                 <FaSave />
                                 Salvar
-                            </botton>
-                            <botton className='btn-cancel'>
+                            </button>
+                            <button className='btn-cancel'>
                                 <MdOutlineCancel />
-                                Cancelar</botton>
+                                Cancelar</button>
                         </div>
 
                     </form>
-                 
+
+
                 </div>
 
 
